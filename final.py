bb16035da8717af1f79664d7e606114571ff6981
Swaraag <swaraag.sistla@gmail.com>
2026-03-09 15:26:21 -0700

    clean missing and irrelavant data and trim training columns to 23 columns total

--- final.py
+++ final.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib as plt
 
 df = pd.read_csv("cumulative_2026.03.09_14.43.47.csv", comment="#")
 df_candidates = df[df["koi_disposition"] == "CANDIDATE"]
 df = df[df["koi_disposition"] != "CANDIDATE"]
+
+print(df["koi_disposition"].value_counts())
+print(df.isnull().sum())
+print(df[(df["kepler_name"].notnull()) & (df["koi_disposition"] == "FALSE POSITIVE")])
